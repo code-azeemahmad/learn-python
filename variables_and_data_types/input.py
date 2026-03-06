4b69e11892f29fc29e7bfec7f13c79fdb1a0bb1a
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-06 20:58:01 +0500

    Taking input in python

---
+++ variables_and_data_types/input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print("Hello", name)
+
+# input() takes input in string data type
+
+val = int(input("Enter your age: "))
+print("Your age is:", val)      
+print(type(val)) 
