8df85561de10fb1236b7d0a191d7a07905bac892
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-06 19:22:13 +0500

    Data types in python

---
+++ variables_and_data_types/type.py
@@ -0,0 +1,19 @@
+old = True
+n = None
+
+str1 = 'azeem'
+str2 = "azeem"
+str3 = '''azeem'''
+
+print(type(old))
+print(type(n))
+
+a = 4
+b = 3
+sum = a + b
+print(sum)
+
+# I am a single line comment
+"""
+hey! I am a multi line comment
+"""
