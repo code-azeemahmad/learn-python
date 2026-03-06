29beca3c549b104f89491a41d9ddcd8028654cfc
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-06 20:14:46 +0500

    operators in python

---
+++ variables_and_data_types/op.py
@@ -0,0 +1,44 @@
+# arithmetic operators
+a = 12
+b = 6
+
+sum = a + b
+diff = a - b
+mod = a % b
+div = a / b
+mul = a * b
+expo = a ** b
+
+print(sum)
+print(diff)
+print(mul)
+print(div)
+print(mod)
+print(expo)
+
+# relational operators
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+
+
+# assignment operators
+num = 10
+num = num + 10
+num += 10
+num *= 10
+num /= 10
+# num **= 10
+
+print("num:", num)
+
+# logical operators
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = False
+print("AND :", val1 and val2)
+print("OR :", val1 or val2)
+print("LOGIC :", (a == b) or (a > b))
