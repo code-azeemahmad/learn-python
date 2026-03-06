15fab499d8f5b30b19506bf84c104bfdd5efe262
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-06 19:15:05 +0500

    Variables in python

---
+++ variables_and_data_types/hello.py
@@ -0,0 +1,16 @@
+print("hello world")
+print ("Azeem")
+print(45)
+print(30 + 20)
+
+name = "azeem"
+age = 21
+price = 45.99
+
+print("My name is :", name)
+print("My age is :", age)
+print("The price is :", price)
+
+print(type(name))
+print(type(age))
+print(type(price))
