d994841de98cc844385dfe85d03f2d6846e4e657
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-07 11:02:00 +0500

    Intro to Sets in python

---
+++ dictionary_and_set/set.py
@@ -0,0 +1,14 @@
+# Set is the colection of unordered items
+# each element in the set must be unique and immutable
+# list and dictionary cannot be stored in a set because they are mutable
+
+collection = {1, 2, 2, 2, 3, 4, "hello", "world"}
+print(collection)       # ignore duplicate items
+print(len(collection))
+print(type(collection))
+
+collektion = {}     # empty dictionary
+collektion = set()  # empty set
+print(type(collektion))
+
+# sets are mutable but elements in set are immutable
