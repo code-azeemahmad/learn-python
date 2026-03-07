e89807d31397f03cb212fd9c082df81068363c81
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-07 13:04:51 +0500

    For loop in python

---
+++ loop/for.py
@@ -0,0 +1,54 @@
+# for sequential traversal
+
+nums = [1, 2, 3, 4, 5, 6]
+tup = (1, 2, 3, 4, 5, 6)
+
+for val in tup:
+    print(val)
+
+
+str = "code-azeem"
+
+for char in str:
+    if (char == "o"):
+        print("o found")
+        break       # prevents else to execute
+    print(char)
+else:
+    print("loop ended")     # exucute when the loop has completed
+    
+    
+'''
+Range()
+    Range functions returns a seqence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number
+    range(start?, stop, step?)
+'''
+
+print(range(5))
+seq = range(5)
+# print(seq[1])
+# print(seq[2])
+
+for i in seq:
+    print(i)
+
+for i in range(10):
+    print(i)
+    
+for k in range(0, 100, 2):
+    print(k)
+    
+# start → included
+# stop  → NOT included
+
+
+"""
+pass
+    pass is a null statement that does nothing. It is used as a placeholder for future code
+"""
+
+for i in range(5):
+    pass
+if ( i < 5):
+    pass
+print("some usefull work")
