c95a163e19d386a1cb6ae5366db7136c0893db2a
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-07 14:13:16 +0500

    find sum of numbers and factorial

--- loop/for.py
+++ loop/for.py
@@ -52,3 +52,19 @@ for i in range(5):
 if ( i < 5):
     pass
 print("some usefull work")
+
+# WAP to find the sum of first n natural numbers
+n = 5
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+print(sum)
+
+# WAP to find the factorial of first n numbers
+n = 5
+i = 1
+fact = 1
+while i <= n:
+    fact *= i
+    i += 1
+print(fact)
