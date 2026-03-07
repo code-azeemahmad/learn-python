67ae947904f66fd70bd45acfd5a9cd351cf49289
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-07 07:17:29 +0500

    conditional statements in python

---
+++ strings_and_conditionals/conditional.py
@@ -0,0 +1,18 @@
+light = "red"
+
+if (light == "red"):
+    print("stop")
+elif (light == "green"):
+    print("go")
+elif (light == "yellow"):
+    print("wait")
+else:
+    print("light is broken")
+
+
+age = 17
+
+if (age >= 18):
+    print("can vote")
+else:
+    print("can't vote") 
