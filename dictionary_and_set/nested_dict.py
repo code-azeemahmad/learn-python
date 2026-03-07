a21900188a666f74a2da9bc4f48df43f28eeb152
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-07 08:54:36 +0500

    Nested dictionaries

---
+++ dictionary_and_set/nested_dict.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "azeem",
+    "subject" : {
+        "chem" : 98,
+        "phys" : 95,
+        "math" : 93,
+    }
+}
+
+print(student)
+print(student["subject"])
+print(student["subject"]["chem"])
