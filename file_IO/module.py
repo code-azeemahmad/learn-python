4101d4f29a94fe409ea64d2703a97e13bb31562e
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-07 16:18:48 +0500

    deleting a file using os module

---
+++ file_IO/module.py
@@ -0,0 +1,10 @@
+# Deleting a File
+# using the os module
+'''
+Module (like a code library) is a file written by another programmer that generally has a funtion we can use
+    import os
+    os.remove(file_name)
+'''
+import os
+os.remove(r"F:\python learning\learn-python\file_IO\hello.txt")
+
