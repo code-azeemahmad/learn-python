88fab64fb53e3b1e2da11dd5120f600f17cac549
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-04 19:14:05 +0500

    Add additional examples for slicing and accessing elements in tea_varieties list

--- chai-aur-python/List_data_type/main.py
+++ chai-aur-python/List_data_type/main.py
@@ -9,3 +9,10 @@
 # pink
 # >>> print(tea_varieties[-1])
 # white
+
+# >>> print(tea_varieties[1:3]) 
+# ['pink', 'green']
+# >>> print(tea_varieties[:2]) 
+# ['black', 'pink']
+# >>> print(tea_varieties[2:])
+# ['green', 'white']
