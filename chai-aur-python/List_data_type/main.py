460316417b274dc631a4cf3d46a27ee610c409ad
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-04 20:01:24 +0500

    Add examples of modifying list elements in tea_varieties

--- chai-aur-python/List_data_type/main.py
+++ chai-aur-python/List_data_type/main.py
@@ -16,3 +16,15 @@
 # ['black', 'pink']
 # >>> print(tea_varieties[2:])
 # ['green', 'white']
+
+# >>> tea_varieties                
+# ['black', 'a', 'l', 'm', 'o', 'n', 'd', 'green', 'herbal']
+# >>> tea_varieties = ["black", "pink", "green", "white"]
+# >>> tea_varieties[1:2] = ["almond"]
+# >>> tea_varieties                  
+# ['black', 'almond', 'green', 'white']
+# >>> tea_varieties[1:3]
+# ['almond', 'green']
+# >>> tea_varieties[1:3] = ["hazelnut", "cashew"]
+# >>> tea_varieties
+# ['black', 'hazelnut', 'cashew', 'white']
