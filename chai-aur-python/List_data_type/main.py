93bd061854a7c7548481e1c3e8ad6465713d5ade
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-04 19:12:21 +0500

    Add examples of list data type with tea varieties in main.py

---
+++ chai-aur-python/List_data_type/main.py
@@ -0,0 +1,11 @@
+# >>> tea_varieties = ["black", "pink", "green", "white"]
+# >>> tea_varieties
+# ['black', 'pink', 'green', 'white']
+# >>> print(tea_varieties)
+# ['black', 'pink', 'green', 'white']
+# >>> print(tea_varieties[0])
+# black
+# >>> print(tea_varieties[1])
+# pink
+# >>> print(tea_varieties[-1])
+# white
