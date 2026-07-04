8525fcd758675f2f0ce6e9daa6f084906046aaa1
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-04 20:39:37 +0500

    Add example code demonstrating dictionary usage and key retrieval

---
+++ chai-aur-python/Dictionary_data_type/main.py
@@ -0,0 +1,17 @@
+# list - order matters
+# dict - order does not matter
+
+# >>> things = {"sky": "blue", "night": "black", "wall": "white"}
+# >>> things
+# {'sky': 'blue', 'night': 'black', 'wall': 'white'}
+# >>> print(things["sky"])
+# blue
+# >>> things.get("night") 
+# 'black'
+# >>> things.get("nights")
+# >>> print(things["ski"]) 
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#     print(things["ski"])
+#           ~~~~~~^^^^^^^
+# KeyError: 'ski'
