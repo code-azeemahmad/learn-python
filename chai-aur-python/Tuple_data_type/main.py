915032744bb4658b6d072c76c89fb8df6a63b77f
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-05 08:31:06 +0500

    Add examples demonstrating tuple indexing and immutability

--- chai-aur-python/Tuple_data_type/main.py
+++ chai-aur-python/Tuple_data_type/main.py
@@ -4,3 +4,16 @@
 
 # >>> print(type(teaTypes))  
 # <class 'tuple'>
+
+# >>> teaTypes[0]
+# 'black'
+# >>> teaTypes[-1]
+# 'pink'
+# >>> teaTypes[1:3]
+# ('green', 'pink')
+# >>> teaTypes[0] = "lemon"
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#     teaTypes[0] = "lemon"
+#     ~~~~~~~~^^^
+# TypeError: 'tuple' object does not support item assignment
