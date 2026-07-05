abca8c5153103d7357c043439856ce0f7b0dccc5
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-05 08:13:17 +0500

    Add example demonstrating dictionary comprehension and clearing a dictionary

--- chai-aur-python/Dictionary_data_type/main.py
+++ chai-aur-python/Dictionary_data_type/main.py
@@ -84,3 +84,10 @@
 # >>> tea_shop["tea"]["honey"] = ["sugar"]
 # >>> tea_shop["tea"]["honey"]            
 # ['sugar']
+
+# >>> squared_num = {x:x**2 for x in range(6)}
+# >>> squared_num
+# {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+# >>> squared_num.clear()
+# >>> squared_num        
+# {}
