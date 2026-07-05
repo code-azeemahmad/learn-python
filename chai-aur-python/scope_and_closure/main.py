1f6141ac7e66096b95659517afef3600c7a3cc71
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-05 18:17:06 +0500

    Add closure and higher-order function examples

--- chai-aur-python/scope_and_closure/main.py
+++ chai-aur-python/scope_and_closure/main.py
@@ -23,3 +23,29 @@ def func3():
 
 # print(func3())  # 12
 # print(x)    # 12
+
+def f1():
+    x = 88
+    def f2():   # closure (f2 takes x = 88 from f1)
+        print(x)
+    # f2()
+    return f2   # reference
+result = f1()
+result()    # execute
+
+
+def outer(num):
+    def inner(x):
+        return x ** num
+    return inner
+
+f = outer(2)    # reference of inner()
+g = outer(3)
+
+# def outer(2):
+#     def inner(x):
+#         return x ** 2
+#     return inner
+
+print(f(3)) # calc square
+print(g(3)) # calc cube
