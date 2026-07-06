5f3ea78d2c151fdbe07d8fc76b8ce58cf287a5f5
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-06 07:05:01 +0500

    Remove unnecessary comments and clean up static method documentation in Car class

--- chai-aur-python/oop/static_method.py
+++ chai-aur-python/oop/static_method.py
@@ -1,11 +1,6 @@
 '''Problem: Add a static method to the Car class 
 that returns a general description of a car.'''
 
-'''
-Problem: Create an ElectricCar class that inherits 
-from the Car class and has an additional attribute
-battery_size
-'''
 class Car:
 
     def __init__(self, brand, model):
