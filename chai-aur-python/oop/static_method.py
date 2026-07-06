c657b2f042a92382ec1799dcc471970ff195c0ee
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-06 07:02:51 +0500

    Add static method to Car class for general car description

---
+++ chai-aur-python/oop/static_method.py
@@ -0,0 +1,24 @@
+'''Problem: Add a static method to the Car class 
+that returns a general description of a car.'''
+
+'''
+Problem: Create an ElectricCar class that inherits 
+from the Car class and has an additional attribute
+battery_size
+'''
+class Car:
+
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    '''A static method is a method that belongs to the class, 
+    but does not need access to the object (self) or the class (cls).'''
+
+    @staticmethod   # independent of objects (can be called through object but should not be)
+    def generalDesc():
+        return "Cars are means of transport"
+        
+myCar = Car("Toyata", "Corolla")
+print(Car.generalDesc())
+
