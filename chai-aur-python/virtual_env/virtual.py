494720241adf27adc478763db5f8570895bd6caa
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-06 19:10:55 +0500

    Add requirements and virtual environment setup instructions

---
+++ chai-aur-python/virtual_env/virtual.py
@@ -0,0 +1,19 @@
+# pip install virtualenv
+
+# cd path/to/your/project
+
+# python -m venv .venv
+
+# .venv\Scripts\activate.bat
+# .venv\Scripts\activate
+
+# deactivate
+
+# pip list
+
+# pip list > requirements.txt
+
+# py -m pip install Django==6.0.6
+ 
+# pip install -r requirements.txt
+
